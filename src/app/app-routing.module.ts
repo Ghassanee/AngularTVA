a8e030345109b3f19f7978061315bbaad81181ec
ghassane <ghassane20a@gmail.com>
2020-03-23 00:09:48 +0100

    societe , type societe declaration ir , declaration tva , taux de ir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,50 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { FactureChargeComponent } from './facture-charge/facture-charge.component';
+import { FactureGainComponent } from './facture-gain/facture-gain.component';
+import { SocieteComponent } from './societe/societe.component';
+import { CreateSocieteComponent } from './societe/create-societe/create-societe.component';
+import {DeclarationIRComponent} from './declaration-ir/declaration-ir.component';
+import {DeclarationTVAComponent} from './declaration-tva/declaration-tva.component';
+import {TauxDeIRComponent} from './taux-de-ir/taux-de-ir.component';
+
+const routes: Routes = [
+  {
+    path: 'FactureCharge',
+    component: FactureChargeComponent
+  },
+  {
+    path: 'FactureGain',
+    component : FactureGainComponent
+
+  },
+  {
+    path: 'Societe',
+    component : SocieteComponent
+
+  },
+
+  {
+    path : 'CreateSocieteComponent',
+    component: CreateSocieteComponent
+  },
+  {
+    path : 'DeclarationIR',
+    component: DeclarationIRComponent
+  },
+  {
+    path: 'DeclarationTVA',
+    component: DeclarationTVAComponent
+  },
+  {
+    path: 'TauxDeIR',
+    component: TauxDeIRComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
