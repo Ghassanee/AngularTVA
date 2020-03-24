1c0ade50f444bb610589ab0ee58d4bf03eed220e
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2020-03-24 15:35:47 +0100

    Adding two pages ( first one : add name of societe and the next one is fill the facturs ( gain and charge ) of the society then go to page of tva for calcul
    Adding two pages ( first one : add name of societe and the next one is fill the facturs ( gain and charge ) of the society then go to page of tva for calcul
    @doraymonit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FactureComponent } from './facture/facture.component';
+import { FactureChargeComponent } from './facture/facture-charge/facture-charge.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'facture',
+    component : FactureComponent
+  },
+  {
+    path: '',
+    component : HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
 import { FactureComponent } from './facture/facture.component';
 import { FactureChargeComponent } from './facture/facture-charge/facture-charge.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { FactureChargeComponent } from './facture/facture-charge/facture-charge.
 
     FactureComponent,
 
-    FactureChargeComponent
+    FactureChargeComponent,
+
+    HomeComponent
   ],
   imports: [
     BrowserModule,
