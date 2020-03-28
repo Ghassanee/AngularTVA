bfded24ffedb5f4bdb1f2a5b9a7c57aa6db5192f
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2020-03-28 15:27:52 +0100

    adding Facture pour simplifier le travai pour les gens qui ont FactureCharge et FactureGain avec un astuce avec JS de typeFacture ( hide/show)

--- src/app/facture/factureF/factureF.component.ts
+++ src/app/facture/factureF/factureF.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FactureService } from 'src/app/controller/service/facture.service';
+import { SocieteService } from 'src/app/controller/service/societe.service';
+import { DeclarationTVAService } from 'src/app/controller/service/declaration-tva.service';
+import { Facture } from 'src/app/controller/model/facture.model';
+import { Societe } from 'src/app/controller/model/societe.model';
+import { DeclarationTVA } from 'src/app/controller/model/declaration-tva.model';
 
 @Component({
   selector: 'app-factureF',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FactureChargeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private factureService : FactureService, private socservice : SocieteService,
+    private declarationTvaService : DeclarationTVAService) { }
 
   ngOnInit(): void {
+    this.factureService.findAll();
+    this.socservice.findAll();
+    this.declarationTvaService.findAll();
   }
+public save(){
+  this.declarationTvaService.save();
+  this.factureService.save();
+
+}
+  public get facture(): Facture {
+
+    return this.factureService.facture;
+  }
+  public get factures(): Array<Facture> {
+
+    return this.factureService.factures;
+  }
+  public get socs(): Array<Societe> {
+
+    return this.socservice.socs;
+  }
+  public get declarationTVA(): DeclarationTVA {
+    return this.declarationTvaService.declarationTVA;
+    }
 
 }
